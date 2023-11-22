f5e44876a47a7baf980a1a57d5d092a324d782aa
Wilder Ap <wilderasprilla321@gmail.com>
2023-11-22 15:24:08 -0500

    Base de estilo

--- Client/my-website/src/components/HomepageFeatures/index.js
+++ Client/my-website/src/components/HomepageFeatures/index.js
@@ -5,7 +5,7 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    Svg: require('@site/static/img/Background.svg').default,
     description: (
       <>
         Docusaurus was designed from the ground up to be easily installed and
@@ -15,7 +15,7 @@ const FeatureList = [
   },
   {
     title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    Svg: require('@site/static/img/yellowgirl.svg').default,
     description: (
       <>
         Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
@@ -24,8 +24,8 @@ const FeatureList = [
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'Team work is Always key',
+    Svg: require('@site/static/img/teamm.svg').default,
     description: (
       <>
         Extend or customize your website layout by reusing React. Docusaurus can
--- Client/my-website/src/pages/index.js
+++ Client/my-website/src/pages/index.js
@@ -6,21 +6,22 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
-
+// Calendars await for you
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
-          {siteConfig.title}
+          {"C4LEND4RS"}
         </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        
+        <p className="hero__subtitle">{"¡Calendars are waiting to be create!"}</p> 
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Create your Schedule
           </Link>
         </div>
       </div>
